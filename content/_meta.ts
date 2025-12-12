b3f468eb75c5cbbdcc37e83c22575ab1201a7088
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 17:21:36 -0800

    Add Docusaurus documentation site

---
+++ content/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  'index': 'Home',
+  'valid-changelog': 'Valid - Changelog',
+  'valid-troubleshooting': 'Valid - Troubleshooting',
+  'valid-advanced-features': 'Valid - Advanced Features',
+  'api-valid-pet-store-api-yaml': {
+    title: 'Valid - Pet Store API (YAML)',
+    type: 'page',
+    href: '/api-docs-valid-pet-store-api-yaml.html'
+  }
+}
