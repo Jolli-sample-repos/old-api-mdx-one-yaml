e45fe1e797944281ada7bb1898e372228342543e
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 17:45:05 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -3,11 +3,6 @@ export default {
   'valid-changelog': 'Valid - Changelog',
   'valid-troubleshooting': 'Valid - Troubleshooting',
   'valid-advanced-features': 'Valid - Advanced Features',
-  'api-valid-pet-store-api-yaml': {
-    title: 'Valid - Pet Store API (YAML)',
-    type: 'page',
-    href: '/api-docs-valid-pet-store-api-yaml.html'
-  },
   'api-reference': {
     title: 'API Reference',
     type: 'page',
